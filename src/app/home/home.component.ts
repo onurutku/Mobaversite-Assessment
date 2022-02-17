0883e5fbd092237ca9fcdf49c5ccbed9e966e57a
onurutku <topaloglu08@gmail.com>
2022-02-18 00:39:14 +0300

    body started

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  popularClicked: boolean = true;
+  recentClicked: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  popular() {
+    this.popularClicked = true;
+    this.recentClicked = false;
   }
-
+  recent() {
+    this.recentClicked = true;
+    this.popularClicked = false;
+  }
 }
