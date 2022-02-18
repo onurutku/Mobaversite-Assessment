c1798792301f2084af961833386b63deac44a0b3
onurutku <topaloglu08@gmail.com>
2022-02-18 19:58:19 +0300

    start page will complete

--- src/app/home/topic-detail/topic-detail.component.ts
+++ src/app/home/topic-detail/topic-detail.component.ts
@@ -13,13 +13,14 @@ export class TopicDetailComponent implements OnInit, OnDestroy {
   topicId: string;
   topic: any;
   topicOwner: any;
+  openReply: string = null;
   constructor(private route: ActivatedRoute, private home: HomeService) {}
 
   ngOnInit(): void {
     this.subs = this.route.params.subscribe((params: Params) => {
       this.topicId = params['id'];
       this.topic = this.home.getTopicsDataById(this.topicId);
-      console.log(this.topic.details.comments[0]);
+      console.log(this.topic.details.comments[1].replies);
 
       this.topicOwner = this.home.getTopicOwnerById(this.topic.ownerId);
       console.log(this.topicOwner);
@@ -27,5 +28,16 @@ export class TopicDetailComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
     this.subs.unsubscribe();
+  }
+  getCommentOwnerInfo(id: string) {
+    return this.home.getTopicOwnerById(id);
+  }
+  openReplies(id: string) {
+    if (this.openReply == null || this.openReply != id) {
+      this.openReply = id;
+    } else {
+      this.openReply = null;
+    }
+    console.log(id);
   }
 }
